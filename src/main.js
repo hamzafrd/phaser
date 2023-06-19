0905a9f80b4164b0eb742956073d862b3e765863
Hamza Firdaus <elhamzafirdaus@gmail.com>
2023-06-19 17:58:20 +0700

    initial commit

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Phaser from 'phaser'
+
+// import HelloWorldScene from './scenes/HelloWorldScene'
+import CoronaBusterScene from './scenes/CoronaBustersScene'
+
+const config = {
+	type: Phaser.AUTO,
+	width: 400,
+	height: 620,
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: 0 }
+		}
+	},
+	scene: [CoronaBusterScene],
+	scale: {
+		mode: Phaser.Scale.FIT,
+		autoCenter: Phaser.Scale.CENTER_BOTH
+	},
+}
+
+export default new Phaser.Game(config)
